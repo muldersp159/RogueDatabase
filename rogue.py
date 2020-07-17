f742f08370a55b763bf5b5e571b96dbf20652245
bradnielsen2981 <38126504+bradnielsen2981@users.noreply.github.com>
2020-07-17 11:08:34 +1000

    updated

--- rogue.py
+++ rogue.py
@@ -4,6 +4,9 @@ database = DatabaseHelper('monsters.db')
 
 results = database.ViewQueryHelper("SELECT * FROM MonsterTable")
 for row in results:
-    print(row['MonsterName'])
+    print("Monster: " + row['MonsterName'])
+    print("Defence: " + str(row['Defence']))
+    print("Health: " + str(row['Health']))
+    print("Points: " + str (row['Points']))
 
 #database.ModifyQueryHelper("DELETE FROM users WHERE userid = ?",(userid,)) 
