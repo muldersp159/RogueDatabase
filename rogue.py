5d89f8155dbc4938695f739805128463cbb4c4b5
bradnielsen2981 <38126504+bradnielsen2981@users.noreply.github.com>
2020-07-17 11:23:05 +1000

    committed

--- rogue.py
+++ rogue.py
@@ -1,12 +1,20 @@
 from interfaces.databaseinterface import DatabaseHelper
+import random
 
 database = DatabaseHelper('monsters.db')
+results = database.ViewQueryHelper("SELECT * FROM MonsterTable") 
+monster = random.choice(results) #python dictionary 
+monstername = monster['MonsterName'] #store the monstername
 
-results = database.ViewQueryHelper("SELECT * FROM MonsterTable")
+print("Welcome to the dungeon of death")
+print("Oh no, you have been attacked!!!")
+print("Monster: " + monster['MonsterName'])
+print("Defence: " + str(monster['Defence']))
+print("Health: " + str(monster['Health']))
+print("Points: " + str (monster['Points']))
+
+results = database.ViewQueryHelper("SELECT * FROM Abilities WHERE MonsterName = ?",(monstername,))
 for row in results:
-    print("Monster: " + row['MonsterName'])
-    print("Defence: " + str(row['Defence']))
-    print("Health: " + str(row['Health']))
-    print("Points: " + str (row['Points']))
+    print(row['AttackName'])
 
 #database.ModifyQueryHelper("DELETE FROM users WHERE userid = ?",(userid,)) 
